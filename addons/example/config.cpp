1d310af4986ac199b7411a229ecb45f4a8eff408
Brett <brett@bmandesigns.com>
2019-02-19 03:38:46 -0600

    update 0.5

--- addons/example/config.cpp
+++ addons/example/config.cpp
@@ -1,14 +1,15 @@
 #include "script_component.hpp"
-class CfgPatches {
-  class ADDON {
-    name = COMPONENT;
-    units[] = {};
-    weapons[] = {};
-    requiredVersion = REQUIRED_VERSION;
-    requiredAddons[] = {};
-    author = "SynixeBrett";
-    VERSION_CONFIG;
-  };
-};
+    class CfgPatches {
+        class ADDON {
+            name = COMPONENT;
+            units[] = {};
+            weapons[] = {};
+            requiredVersion = REQUIRED_VERSION;
+            requiredAddons[] = {};
+            author = "SynixeBrett";
+            VERSION_CONFIG;
+        };
+    };
+
 
 #include "CfgEventHandlers.hpp"
--- addons/main/config.cpp
+++ addons/main/config.cpp
@@ -1,12 +1,12 @@
 #include "script_component.hpp"
-class CfgPatches {
-  class ADDON {
-    name = COMPONENT;
-    units[] = {};
-    weapons[] = {};
-    requiredVersion = REQUIRED_VERSION;
-    requiredAddons[] = {};
-    author = "SynixeBrett";
-    VERSION_CONFIG;
-  };
-};
+    class CfgPatches {
+        class ADDON {
+            name = COMPONENT;
+            units[] = {};
+            weapons[] = {};
+            requiredVersion = REQUIRED_VERSION;
+            requiredAddons[] = {};
+            author = "SynixeBrett";
+            VERSION_CONFIG;
+        };
+    };
